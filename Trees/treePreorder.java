150d8440536f4660a9e525db43aeabfbd81f8ebc
Om Prakash Parida <179730524+omprakashparida@users.noreply.github.com>
2026-06-25 20:27:35 +0530

    Inorder and Postoder Traversal

--- Trees/treePreorder.java
+++ Trees/treePreorder.java
@@ -1,5 +1,5 @@
 public class treePreorder {
-    static class Node {
+    public static class Node {
         int data;
         Node left;
         Node right;
@@ -25,12 +25,40 @@ public class treePreorder {
             newNode.right = BuildTree(nodes);
             return newNode;
         }
+
+        public static void preOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public static void inOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+
+        public static void postOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
 
     public static void main(String[] args) {
-        int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.BuildTree(nodes);
-        System.out.println(root.data);
-}
+        tree.postOrder(root);
+        // System.out.println(root.data);
+    }
 }
