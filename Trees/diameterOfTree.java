d818153de55e0629da2a218bef8ffa74d86be422
Om Prakash Parida <179730524+omprakashparida@users.noreply.github.com>
2026-06-26 14:00:12 +0530

    Diameter Of Tree

---
+++ Trees/diameterOfTree.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class diameterOfTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    public static int diameterOfTree(Node root){
+        if (root == null) {
+            return 0;
+        }
+        int leftDiameter = diameterOfTree(root.left);
+        int rightDiameter = diameterOfTree(root.right);
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int selfDiameter = leftHeight+rightHeight+1;
+        int max = Math.max(rightDiameter, leftDiameter);
+        return Math.max(max, selfDiameter);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.right.left.right = new Node(8);
+        System.out.println(diameterOfTree(root));
+    }
+}
