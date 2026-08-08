ae91296d5a4c7f5062cd2fe9b5b39bfce09dd4b4
Om Prakash Parida <179730524+omprakashparida@users.noreply.github.com>
2026-08-08 23:54:10 +0530

    Minimum Bit Flips

---
+++ bitManipulation/MinimumBitFlips.java
@@ -0,0 +1,18 @@
+public class MinimumBitFlips {
+
+    public static int minBitFlips(int start, int goal) {
+        return Integer.bitCount(start ^ goal);
+    }
+
+    public static void main(String[] args) {
+
+        int start = 10;
+        int goal = 7;
+
+        int result = minBitFlips(start, goal);
+
+        System.out.println("Start: " + start);
+        System.out.println("Goal: " + goal);
+        System.out.println("Minimum bit flips: " + result);
+    }
+}
