6be5a3e9e89d1280366d3987d1cb8f97b86ac7a5
Om Prakash Parida <179730524+omprakashparida@users.noreply.github.com>
2026-08-17 23:45:27 +0530

    Level Order Traversal

---
+++ Trees/LevelOrderTraversal.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class LevelOrderTraversal {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node currData = q.remove();
+
+            if (currData == null) {
+                System.out.println();
+
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currData.data + " ");
+
+                if (currData.left != null) {
+                    q.add(currData.left);
+                }
+
+                if (currData.right != null) {
+                    q.add(currData.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        levelOrder(root);
+    }
+}
