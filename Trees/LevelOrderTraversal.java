abbffc8075eb6303d9feaf7bb5466a55f97be8ed
Om Prakash Parida <179730524+omprakashparida@users.noreply.github.com>
2026-08-18 23:52:36 +0530

    Level Order Traversal

--- Trees/LevelOrderTraversal.java
+++ Trees/LevelOrderTraversal.java
@@ -50,7 +50,6 @@ public class LevelOrderTraversal {
 
     public static void main(String[] args) {
 
-
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
